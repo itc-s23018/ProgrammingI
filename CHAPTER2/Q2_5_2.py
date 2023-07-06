a81e7aca0c12953ae6ff22f4ed41651883ccb458
Seishiro Tokuzato <s23018@std.it-college.ac.jp>
2023-07-06 06:21:21 +0000

    chap5

--- CHAPTER2/Q2_5_2.py
+++ CHAPTER2/Q2_5_2.py
@@ -2,11 +2,6 @@ w = "○ "
 b = "● "
 answer = ""
 for i in range(4):
-    for j in range(4):
-        if i == j:
-            answer += w
-        else:
-            answer += b
-    answer += "\n"
-
+    row = (w if i == j else b for j in range(4))
+    answer += "".join(row) + "\n"
 print(answer)
