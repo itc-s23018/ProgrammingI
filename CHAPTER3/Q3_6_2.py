95945c12fb7f7fa5d01685268463a1f4bd793378
Seishiro Tokuzato <s23018@std.it-college.ac.jp>
2023-07-18 07:57:11 +0000

    chap3_6

--- CHAPTER3/Q3_6_2.py
+++ CHAPTER3/Q3_6_2.py
@@ -2,17 +2,24 @@ import random
 
 numbers = [str(i) for i in range(0, 10)]
 num4 = "".join(random.sample(numbers, k=4))
+
 while True:
     val = input()
     if val == num4:
         break
     if len(val) != 4:
-        print("input 4 numbers. ")
+        print("4つの数字を入力してください。")
         continue
     answer = ""
+    X_count = 0
+
     for i in range(4):
         if num4[i] == val[i]:
             answer += num4[i]
         else:
             answer += "X"
-    print("->" + answer)
+            X_count += 1
+
+    remaining_count = X_count
+    print("-> " + answer + '\n' + "あと" + str(remaining_count) + "個で正解です")
+
