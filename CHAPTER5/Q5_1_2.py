a9ff6fc7fc0b26e651112274b4302b31f08e7f67
Seishiro Tokuzato <s23018@std.it-college.ac.jp>
2023-08-08 05:48:23 +0000

    chap5_1

---
+++ CHAPTER5/Q5_1_2.py
@@ -0,0 +1,6 @@
+stack = []
+stack.append(1)
+stack.append(2)
+stack.pop()
+stack.pop()
+print(stack)
---
+++ CHAPTER5/Q5_1_3.py
@@ -0,0 +1,7 @@
+queue = [6, 3, 9]
+queue.append(1)
+queue.append(2)
+print(queue)
+queue.pop(1)
+queue.pop(2)
+print(queue)
