1a5a32b7bfd66f665c326b0c48bfae71ed8cade6
itc-s23018 <s23018@std.it-college.ac.jp>
2023-09-05 13:08:18 +0000

    chap4_8

--- CHAPTER4/Q4_8_1.py
+++ CHAPTER4/Q4_8_1.py
@@ -6,3 +6,13 @@ def show_begin_end(func):
         return result
 
     return deco_func
+import time
+
+
+@show_begin_end
+def sleep_for_a_while():
+    print("Sleeping..")
+    time.sleep(2)
+    print("Done Sleeping")
+
+print(sleep_for_a_while())
